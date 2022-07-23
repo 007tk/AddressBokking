3e363c6c1f49ab21d6bfe30baeeb40e3ce2c2464
Tikane Kgoatla <tikane@work.za.com>
2022-07-23 22:07:26 +0200

    Add Address booking Api

---
+++ AddressBooking.Api/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using AddressBooking.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBooking.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+    }
+}
