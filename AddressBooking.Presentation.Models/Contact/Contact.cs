eed7dcdbefb5aa0ad34f113ddd1fcf21ae02af6d
Tikane Kgoatla <tikane@work.za.com>
2022-07-24 18:13:29 +0200

    Add Presentaion.Model project.

---
+++ AddressBooking.Presentation.Models/Contact/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooking.Presentation.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Name")]
+        public string ContactName { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Surname")]
+        public string ContactSurname { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Contact Number")]
+        public int ContactNumber { get; set; }
+    }
+}
