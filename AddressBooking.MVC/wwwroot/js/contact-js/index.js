47be4b5657c44a9ce4b231dfa77cc5ace8174427
Tikane Kgoatla <tikane@work.za.com>
2022-08-10 21:32:10 +0200

    Add export to csv button

--- AddressBooking.MVC/wwwroot/js/contact-js/index.js
+++ AddressBooking.MVC/wwwroot/js/contact-js/index.js
@@ -1,5 +1,5 @@
-﻿function deleteContact(data){
-    var actionUrl = "https://localhost:44382/api/Contact/DeleteContact?id="+data;
+﻿function deleteContact(data) {
+    var actionUrl = "https://localhost:44382/api/Contact/DeleteContact?id=" + data;
 
     $.ajax({
         url: actionUrl,
@@ -11,4 +11,39 @@
             $('#contactTable').DataTable().ajax.reload();
         }
     });
+}
+
+function htmlToCSV(html, filename) {
+    var data = [];
+    var rows = document.querySelectorAll("table tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+
+        for (var j = 0; j < cols.length; j++) {
+            row.push(cols[j].innerText);
+        }
+
+        data.push(row.join(","));
+    }
+
+    downloadCSVFile(data.join("\n"), filename);
+}
+
+function downloadCSVFile(csv, filename) {
+    var csv_file, download_link;
+
+    csv_file = new Blob([csv], { type: "text/csv" });
+
+    download_link = document.createElement("a");
+
+    download_link.download = filename;
+
+    download_link.href = window.URL.createObjectURL(csv_file);
+
+    download_link.style.display = "none";
+
+    document.body.appendChild(download_link);
+
+    download_link.click();
 }
