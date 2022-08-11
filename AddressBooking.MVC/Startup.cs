ab036d3f133aadb5e4c1ecfe78d940375dfd5f62
Tikane Kgoatla <tikane@work.za.com>
2022-08-11 19:56:29 +0200

    Migrate db on startup

--- AddressBooking.MVC/Startup.cs
+++ AddressBooking.MVC/Startup.cs
@@ -60,6 +60,14 @@ namespace AddressBooking.MVC
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Migrate db on startup.
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<AddressBookingDbContext>();
+                context.Database.EnsureCreated();
+            }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCors("MyPolicy");
