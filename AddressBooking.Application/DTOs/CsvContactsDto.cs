239c5b185240df1c1e4188fd88da0b61cb411980
Tikane Kgoatla <tikane@work.za.com>
2022-08-11 17:53:14 +0200

    Add CsvContactsDto

---
+++ AddressBooking.Application/DTOs/CsvContactsDto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBooking.Application.DTOs
+{
+    public class CsvContactsDto
+    {
+        /// <summary>
+        /// Get & set contact name.
+        /// </summary>
+        public string ContactName { get; set; }
+
+        /// <summary>
+        /// Get & set contact surname.
+        /// </summary>
+        public string ContactSurname { get; set; }
+
+
+        /// <summary>
+        /// Get & set Age
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Get & set date of birth.
+        /// </summary>
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        /// <summary>
+        /// Get & set contact number.
+        /// </summary>
+        public string ContactNumber { get; set; }
+
+        /// <summary>
+        /// Get & set address
+        /// </summary>
+        public string Address { get; set; }
+
+    }
+}
